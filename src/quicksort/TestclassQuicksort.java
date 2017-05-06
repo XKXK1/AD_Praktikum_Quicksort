6f91b720dc4b38db206bd08974e041ff7d98707b
derya1 <deryauyargil@hotmail.de>
2017-05-06 17:33:54 +0200

    tests ueberarbeitet

--- src/quicksort/TestclassQuicksort.java
+++ src/quicksort/TestclassQuicksort.java
@@ -16,11 +16,13 @@ public class TestclassQuicksort {
 
 		befuelleListe1(zahlen1);
 		befuelleListe2(zahlen2);
-		befuelleListe3(zahlen3);
+		zahlen3 =befuelleListe3(zahlen3);
 
-		int zahlen1erwartet[] = zahlen1;
-		int zahlen2erwartet[] = zahlen2;
-		int zahlen3erwartet[] = zahlen3;
+		int zahlenerwartet[] = new int[10000];
+
+		
+		System.arraycopy(zahlen1, 0, zahlenerwartet, 0, zahlen1.length);
+
 
 		Quicksort sorter1 = new Quicksort();
 
@@ -28,9 +30,18 @@ public class TestclassQuicksort {
 		sorter1.quickSort(zahlen2, 0, zahlen2.length - 1, 1);
 		sorter1.quickSort(zahlen3, 0, zahlen3.length - 1, 1);
 
-		assertEquals(zahlen1erwartet, zahlen1);
-		assertEquals(zahlen2erwartet, zahlen2);
-		assertEquals(zahlen3erwartet, zahlen3);
+		
+		
+		for(int i = 0; i < zahlen1.length; i++){
+			assertEquals(zahlenerwartet[i], zahlen1[i]);
+			}
+		for(int i = 0; i < zahlen2.length; i++){
+			assertEquals(zahlenerwartet[i], zahlen2[i]);
+			}
+		for(int i = 0; i < zahlen3.length; i++){
+			assertEquals(zahlenerwartet[i], zahlen3[i]);
+			}
+			
 
 	}
 
@@ -42,11 +53,13 @@ public class TestclassQuicksort {
 
 		befuelleListe1(zahlen1);
 		befuelleListe2(zahlen2);
-		befuelleListe3(zahlen3);
+		zahlen3 =befuelleListe3(zahlen3);
 
-		int zahlen1erwartet[] = zahlen1;
-		int zahlen2erwartet[] = zahlen2;
-		int zahlen3erwartet[] = zahlen3;
+		int zahlenerwartet[] = new int[10000];
+
+		
+		System.arraycopy(zahlen1, 0, zahlenerwartet, 0, zahlen1.length);
+
 
 		Quicksort sorter1 = new Quicksort();
 
@@ -54,37 +67,57 @@ public class TestclassQuicksort {
 		sorter1.quickSort(zahlen2, 0, zahlen2.length - 1, 2);
 		sorter1.quickSort(zahlen3, 0, zahlen3.length - 1, 2);
 
-		assertEquals(zahlen1erwartet, zahlen1);
-		assertEquals(zahlen2erwartet, zahlen2);
-		assertEquals(zahlen3erwartet, zahlen3);
+		
+		
+		for(int i = 0; i < zahlen1.length; i++){
+			assertEquals(zahlenerwartet[i], zahlen1[i]);
+			}
+		for(int i = 0; i < zahlen2.length; i++){
+			assertEquals(zahlenerwartet[i], zahlen2[i]);
+			}
+		for(int i = 0; i < zahlen3.length; i++){
+			assertEquals(zahlenerwartet[i], zahlen3[i]);
+			}
+			
 
 	}
 
 	@Test
 	public void sortierenpivot3() {
-		int zahlen1[] = new int[10000];
-		int zahlen2[] = new int[10000];
-		int zahlen3[] = new int[10000];
 
-		befuelleListe1(zahlen1);
-		befuelleListe2(zahlen2);
-		befuelleListe3(zahlen3);
+			int zahlen1[] = new int[10000];
+			int zahlen2[] = new int[10000];
+			int zahlen3[] = new int[10000];
 
-		int zahlen1erwartet[] = zahlen1;
-		int zahlen2erwartet[] = zahlen2;
-		int zahlen3erwartet[] = zahlen3;
+			befuelleListe1(zahlen1);
+			befuelleListe2(zahlen2);
+			zahlen3 =befuelleListe3(zahlen3);
 
-		Quicksort sorter1 = new Quicksort();
+			int zahlenerwartet[] = new int[10000];
 
-		sorter1.quickSort(zahlen1, 0, zahlen1.length - 1, 3);
-		sorter1.quickSort(zahlen2, 0, zahlen2.length - 1, 3);
-		sorter1.quickSort(zahlen3, 0, zahlen3.length - 1, 3);
+			
+			System.arraycopy(zahlen1, 0, zahlenerwartet, 0, zahlen1.length);
 
-		assertEquals(zahlen1erwartet, zahlen1);
-		assertEquals(zahlen2erwartet, zahlen2);
-		assertEquals(zahlen3erwartet, zahlen3);
 
-	}
+			Quicksort sorter1 = new Quicksort();
+
+			sorter1.quickSort(zahlen1, 0, zahlen1.length - 1, 3);
+			sorter1.quickSort(zahlen2, 0, zahlen2.length - 1, 3);
+			sorter1.quickSort(zahlen3, 0, zahlen3.length - 1, 3);
+
+			
+			
+			for(int i = 0; i < zahlen1.length; i++){
+				assertEquals(zahlenerwartet[i], zahlen1[i]);
+				}
+			for(int i = 0; i < zahlen2.length; i++){
+				assertEquals(zahlenerwartet[i], zahlen2[i]);
+				}
+			for(int i = 0; i < zahlen3.length; i++){
+				assertEquals(zahlenerwartet[i], zahlen3[i]);
+				}
+				
+		}
 
 	/**
 	 * Fuellt das Array mit Zahlen 1 bis Listenlaenge.
@@ -93,7 +126,7 @@ public class TestclassQuicksort {
 	 * @return
 	 */
 	public int[] befuelleListe1(int liste[]) {
-		for (int i = 1; i < liste.length; i++) {
+		for (int i = 1; i <=liste.length; i++) {
 			liste[i - 1] = i;
 		}
 		return liste;
@@ -106,8 +139,8 @@ public class TestclassQuicksort {
 	 * @return
 	 */
 	public int[] befuelleListe2(int liste[]) {
-		for (int i = liste.length - 1; i > 0; i--) {
-			liste[i] = i;
+		for (int i = liste.length; i > 0; i--) {
+			liste[i-1] = i;
 		}
 		return liste;
 	}
